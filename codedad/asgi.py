acbcdbc982150296f5754b1f61965f014a154ab5
anasali2193 <anasali2193@gmail.com>
2020-05-02 21:52:20 +0500

    Initial commit

---
+++ codedad/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for codedad project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codedad.settings')
+
+application = get_asgi_application()
