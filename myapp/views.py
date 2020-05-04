dab67bc48474e16062da4e4c27c1727f0d5688e8
anasali2193 <anasali2193@gmail.com>
2020-05-04 08:56:58 +0500

    fetch data from search field

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,17 @@
+import requests
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup
 
 # Create your views here.
+
 
 def home(request):
     return render(request, 'base.html')
 
 
 def new_search(request):
-    return render(request, 'myapp/new_search.html')
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'myapp/new_search.html', stuff_for_frontend)
